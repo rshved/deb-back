5a57e5863522716d068fbfab0c8c705c84bdb78f
Roman Shvietsov <shvietsovroman@gmail.com>
2022-04-05 00:58:22 +0300

    get deck from api and send to front

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const app = require('express')()
 const http = require('http').createServer(app)
+const axios = require('axios')
 const io = require('socket.io')(http, {
   cors: {
     origins: ['http://localhost:3000']
@@ -10,11 +11,31 @@ app.get('/', (req, res) => {
   res.send('Deberts game')
 })
 
+let cards = null;
+
+(async function getCards () {
+  try {
+    const { data: deck } = await axios('https://deckofcardsapi.com/api/deck/new/shuffle/?cards=AS,7S,8S,9S,10S,JS,QS,KS,AD,7D,8D,9D,' +
+      '10D,JD,QD,KD,AC,7C,8C,9C,10C,JC,QC,KC,AH,7H,8H,9H,10H,JH,QH,KH')
+
+    if (deck.deck_id) {
+      try {
+        const { data } = await axios (`https://deckofcardsapi.com/api/deck/${deck.deck_id}/draw/?count=28`)
+        cards = data.cards
+      } catch (e) {
+        console.error(e)
+      }
+    }
+  } catch (error) {
+    console.error(error)
+  }
+})()
+
 io.on('connection', socket => {
   console.log(`user connected, ${socket.id}`)
 
   socket.on('disconnect', () => {
-    console.log('user disconnected')
+    console.log(`user disconnected, ${socket.id}`)
   })
 
   socket.on('message', (msg, id) => {
@@ -25,6 +46,7 @@ io.on('connection', socket => {
     }
   })
 
+  socket.emit('cards', cards)
 })
 
 http.listen(8000, () => {
